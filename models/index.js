029a8eac68e5f25c73881a914777716607019091
rhazama <rhazama01@gmail.com>
2022-05-17 15:41:33 -0700

    adding to models folder

--- models/index.js
+++ models/index.js
@@ -1,3 +1,33 @@
 const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.belongsTo(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+modeule.exports = { User, Post, Comment };
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,9 +2,29 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        comment_text: '',
-        user_id: '',
-        post_id: ''
+        comment_text: 'I agree with this article',
+        user_id: '3',
+        post_id: '1'
+    }, {
+        comment_text: 'Great article',
+        user_id: '3',
+        post_id: '2'
+    }, {
+        comment_text: 'I disagree',
+        user_id: '2',
+        post_id: '3'
+    }, {
+        comment_text: 'everythingTech Rocks',
+        user_id: '1',
+        post_id: '4'
+    }, {
+        comment_text: 'Thanks for the information',
+        user_id: '7',
+        post_id: '5'
+    }, {
+        comment_text: 'Love this article',
+        user_id: '5',
+        post_id: '6'
     },
 ];
 
