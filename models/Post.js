f8a023c3635d6f7385420e21d357411a2b4d0cc3
rhazama <rhazama01@gmail.com>
2022-05-17 15:50:16 -0700

    adding to models folder

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,32 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {};
 
-Post.init();
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    }, {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
 
 module.exports = Post;
