29a3d0728c8d2e24e6fff316537af3613d9229c5
rhazama <rhazama01@gmail.com>
2022-05-18 12:35:56 -0700

    adding login and log out js

---
+++ public/javascript/login.js
@@ -0,0 +1,53 @@
+async function loginForHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function signForHandler(event) {
+    event.preventDefault();
+    
+    const username = document.querySelector('#username-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginForHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signForHandler);
