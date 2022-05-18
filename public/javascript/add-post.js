31b2adf55e10c31bb8ecabdba5175b6bbaa4cef1
rhazama <rhazama01@gmail.com>
2022-05-18 12:41:41 -0700

    add post js updated

---
+++ public/javascript/add-post.js
@@ -0,0 +1,18 @@
+async function newFormHandler(event) {
+  
+  
+  
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({title, post_text}),
+        headers: { 'Content-Type:': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
